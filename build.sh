1125e592922a94290ebaa5cac2f4c20779651268
科君 <kejun.hch@qq.com>
2026-09-20 10:59:05 +0800

    feat: workflow

--- build.sh
+++ build.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
-PACKAGES="bash \
-vim \
-openssh-sftp-server \
-tcpdump \
-luci-proto-wireguard \
-luci-theme-argon \
-luci-i18n-base-zh-cn \
-luci-i18n-firewall-zh-cn \
-luci-i18n-package-manager-zh-cn \
-luci-i18n-attendedsysupgrade-zh-cn \
-luci-i18n-wol-zh-cn \
-luci-i18n-ddns-go-zh-cn \
-luci-i18n-ttyd-zh-cn \
-luci-i18n-frpc-zh-cn \
-luci-i18n-passwall-zh-cn \
-luci-i18n-natmap-zh-cn
-"
+set -euo pipefail
 
-make image PACKAGES="$PACKAGES" ROOTFS_PARTSIZE="$ROOTFS_SIZE"
+PACKAGES=(
+  bash
+  vim
+  openssh-sftp-server
+  tcpdump
+  luci-proto-wireguard
+  luci-theme-argon
+  luci-i18n-base-zh-cn
+  luci-i18n-firewall-zh-cn
+  luci-i18n-package-manager-zh-cn
+  luci-i18n-attendedsysupgrade-zh-cn
+  luci-i18n-wol-zh-cn
+  luci-i18n-ddns-go-zh-cn
+  luci-i18n-ttyd-zh-cn
+  luci-i18n-frpc-zh-cn
+  luci-i18n-passwall-zh-cn
+  luci-i18n-natmap-zh-cn
+)
+
+make image PACKAGES="${PACKAGES[*]}" ROOTFS_PARTSIZE="$ROOTFS_SIZE"
